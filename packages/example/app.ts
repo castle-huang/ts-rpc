abb910273ff2a88c0877b72bf3ec961b896e3ea6
david <you@example.com>
2025-11-12 23:07:23 +0800

    fix

--- packages/example/app.ts
+++ packages/example/app.ts
@@ -2,22 +2,22 @@ import express, {Request, Response} from 'express';
 
 import {HttpTransport} from "@ts-rpc/core";
 // 显式导入所有服务，确保它们被编译
-// import fs from 'fs';
-// import path from 'path';
-//
-// async function importAllServices() {
-//     const serviceDir = path.join(__dirname, 'src', 'services');
-//     const files = fs.readdirSync(serviceDir);
-//
-//     for (const file of files) {
-//         if (file.endsWith('.ts') || file.endsWith('.js')) {
-//             await import(path.join(serviceDir, file));
-//         }
-//     }
-// }
+import fs from 'fs';
+import path from 'path';
+
+async function importAllServices() {
+    const serviceDir = path.join(__dirname, 'src');
+    const files = fs.readdirSync(serviceDir);
+
+    for (const file of files) {
+        if (file.endsWith('.ts') || file.endsWith('.js')) {
+            await import(path.join(serviceDir, file));
+        }
+    }
+}
 
 async function startServer() {
-    // await importAllServices();
+    await importAllServices();
     const server = new HttpTransport();
     const port = parseInt(process.env.PORT || '3000');
     await server.start(port, ['src', 'packages/example/src']);
